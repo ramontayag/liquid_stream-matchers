43f5f5ed3541874b0f1debf16bb43c4aaeceb60e
Ramon Tayag <ramon@tintin.domain.name>
2014-05-17 13:23:20 +0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in liquid_stream-matchers.gemspec
+gemspec
---
+++ lib/liquid_stream/matchers.rb
@@ -0,0 +1,7 @@
+require "liquid_stream/matchers/version"
+
+module LiquidStream
+  module Matchers
+    # Your code goes here...
+  end
+end
